594e95ef2ee2f2938e9e63b92ec8e0d16cec0786
tangzhang <it_tangzhang@163.com>
2018-12-18 12:28:08 +0800

    12-18

--- src/main/resources/static/js/util.js
+++ src/main/resources/static/js/util.js
@@ -1,18 +1,4 @@
 
-var layer = new Object();
-/**
- * 弹出框设置
- */
-layer.msg = function (text) {
-    layui.use('layer', function(){
-        var layer = layui.layer;
-        // layer.config({
-        //     extend: 'util.css',
-        //     skin:'layer-msg'
-        // });
-        layer.msg(text);
-    });
-}
 /**
  * 开启图片懒加载
  */
@@ -21,3 +7,33 @@ layui.use('flow', function(){
     //当你执行这样一个方法时，即对页面中的全部带有lay-src的img元素开启了懒加载（当然你也可以指定相关img）
     flow.lazyimg();
 });
+
+const layer = new Object();
+/**
+ * 弹出框设置
+ */
+layer.msg = function (text) {
+    layui.use('layer', function(){
+        var l = layui.layer;
+        // layer.config({
+        //     extend: 'util.css',
+        //     skin:'layer-msg'
+        // });
+        l.msg(text);
+    });
+}
+/**
+ *静态框
+ * @param jquery_attr  静态框的对应jquery对象
+ */
+layer.openModal = function(jquery_attr){
+    layui.use('layer', function() {
+        var l = layui.layer;
+        l.open({
+            type: 1,
+            content: jquery_attr,
+            shadeClose:true
+            //这里content是一个DOM，注意：最好该元素要存放在body最外层，否则可能被其它的相对元素所影响
+        });
+    });
+}
