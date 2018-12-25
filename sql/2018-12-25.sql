b704b5061d70a4526e0e0c61308df95cf55512d9
tangzhang <it_tangzhang@163.com>
2018-12-25 18:02:54 +0800

    12-25

--- sql/2018-12-25.sql
+++ sql/2018-12-25.sql
@@ -23,9 +23,9 @@ CREATE TABLE IF NOT EXISTS `tbl_menu` (
   `menu_logo` varchar(255) DEFAULT '' COMMENT '菜单的logo 图片地址',
   `level` int(11) DEFAULT '1' COMMENT '菜单的排序，从上到小 1>',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='左侧一级菜单';
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='左侧一级菜单';
 
--- 正在导出表  ob_admin.tbl_menu 的数据：~2 rows (大约)
+-- 正在导出表  ob_admin.tbl_menu 的数据：~0 rows (大约)
 DELETE FROM `tbl_menu`;
 /*!40000 ALTER TABLE `tbl_menu` DISABLE KEYS */;
 INSERT INTO `tbl_menu` (`id`, `name`, `menu_type`, `menu_logo`, `level`) VALUES
@@ -41,14 +41,15 @@ CREATE TABLE IF NOT EXISTS `tbl_menu_project` (
   `mid` int(11) DEFAULT '0' COMMENT '菜单id',
   `oid` int(11) DEFAULT '0' COMMENT '项目id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
--- 正在导出表  ob_admin.tbl_menu_project 的数据：~5 rows (大约)
+-- 正在导出表  ob_admin.tbl_menu_project 的数据：~2 rows (大约)
 DELETE FROM `tbl_menu_project`;
 /*!40000 ALTER TABLE `tbl_menu_project` DISABLE KEYS */;
 INSERT INTO `tbl_menu_project` (`id`, `mid`, `oid`) VALUES
 	(10, 14, 1),
-	(13, 13, 1);
+	(13, 13, 1),
+	(14, 15, 1);
 /*!40000 ALTER TABLE `tbl_menu_project` ENABLE KEYS */;
 
 
@@ -60,9 +61,9 @@ CREATE TABLE IF NOT EXISTS `tbl_menu_second` (
   `menu_type` tinyint(4) DEFAULT '1' COMMENT '菜单的类型，主要区别管理员和客户，因为管理员菜单不需要和账号匹配  1 管理员 2客户',
   `menu_parent` int(11) DEFAULT '0' COMMENT '上级菜单',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COMMENT='左侧二级菜单表';
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COMMENT='左侧二级菜单表';
 
--- 正在导出表  ob_admin.tbl_menu_second 的数据：~6 rows (大约)
+-- 正在导出表  ob_admin.tbl_menu_second 的数据：~4 rows (大约)
 DELETE FROM `tbl_menu_second`;
 /*!40000 ALTER TABLE `tbl_menu_second` DISABLE KEYS */;
 INSERT INTO `tbl_menu_second` (`id`, `name`, `url`, `menu_type`, `menu_parent`) VALUES
@@ -89,15 +90,13 @@ CREATE TABLE IF NOT EXISTS `tbl_object` (
   `user_id` int(11) DEFAULT '0' COMMENT '项目-用户id',
   `status` tinyint(1) DEFAULT '1' COMMENT '项目状态1 正常 2 到期 3放弃',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COMMENT='项目记录表';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='项目记录表';
 
--- 正在导出表  ob_admin.tbl_object 的数据：~5 rows (大约)
+-- 正在导出表  ob_admin.tbl_object 的数据：~1 rows (大约)
 DELETE FROM `tbl_object`;
 /*!40000 ALTER TABLE `tbl_object` DISABLE KEYS */;
 INSERT INTO `tbl_object` (`id`, `owner`, `name`, `logo`, `descr`, `create_time`, `hold_date`, `hold_cost`, `hold_ins`, `user_id`, `status`) VALUES
-	(1, '创荣科技', '全V健康', 'http://ob-admin.oss-cn-beijing.aliyuncs.com/picture/15520449931/1545282462028_64.jpg?x-oss-process=style/ob-picture-style', '创荣科技项目....', '2018-12-20 10:06:36', '2019-04-20 16:00:43', 0, '全职维护', 0, 1),
-	(12, '1', '1', 'http://p4.so.qhmsg.com/bdr/_240_/t0167d665543136be39.jpg', '1', '2018-12-20 15:53:51', '2019-01-20 16:02:22', 1, '1', 0, 1),
-	(13, '2', '2', 'http://p0.so.qhmsg.com/bdr/_240_/t01c8e252970741d135.jpg', '2', '2018-12-20 15:54:12', '2019-02-20 16:02:29', 2, '2', 0, 1);
+	(1, '创荣科技', '全V健康', 'http://ob-admin.oss-cn-beijing.aliyuncs.com/picture/15520449931/1545282462028_64.jpg?x-oss-process=style/ob-picture-style', '全V健康就诊卡管理', '2018-12-20 10:06:36', '2019-04-20 16:00:43', 0, '全职维护', 12, 1);
 /*!40000 ALTER TABLE `tbl_object` ENABLE KEYS */;
 
 
@@ -107,15 +106,16 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
   `username` varchar(50) DEFAULT '0',
   `password` varchar(255) DEFAULT '123456',
   `level` tinyint(1) DEFAULT '2' COMMENT '账号类型1总管理员  2客户',
+  `prev_oid` int(11) DEFAULT NULL COMMENT '上一次打开的项目ID_多个项目_记住我登录',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 -- 正在导出表  ob_admin.tbl_user 的数据：~2 rows (大约)
 DELETE FROM `tbl_user`;
 /*!40000 ALTER TABLE `tbl_user` DISABLE KEYS */;
-INSERT INTO `tbl_user` (`id`, `username`, `password`, `level`) VALUES
-	(10, '15520449931', 'f3c4cc9c1d0903b690f668f9e401ab34', 1),
-	(12, '17781694763', '4280d89a5a03f812751f504cc10ee8a5', 2);
+INSERT INTO `tbl_user` (`id`, `username`, `password`, `level`, `prev_oid`) VALUES
+	(10, '15520449931', 'f3c4cc9c1d0903b690f668f9e401ab34', 1, NULL),
+	(12, '17781694763', '4280d89a5a03f812751f504cc10ee8a5', 2, 1);
 /*!40000 ALTER TABLE `tbl_user` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
